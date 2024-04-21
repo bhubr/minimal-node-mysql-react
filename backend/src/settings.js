a89d2d0ad66643f733061ddaf1ff41d8b883b268
Benoît Hubert <benoithubert@gmail.com>
2024-04-21 17:15:28 +0200

    Dockerize backend

--- backend/src/settings.js
+++ backend/src/settings.js
@@ -2,16 +2,29 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const db = {
+  host: process.env.DB_HOST ?? "localhost",
+  port: process.env.DB_PORT ?? 3306,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+};
+
+const obfuscateEnd = (str, obfuscateFrom = 4) =>
+  str.slice(0, obfuscateFrom) + "*".repeat(str.length - obfuscateFrom);
+
+// dump host, port, db, user, obfuscated password
+// as a connection string
+console.log(
+  `mysql://${db.user}:${obfuscateEnd(db.password)}@${db.host}:${db.port}/${
+    db.database
+  }`
+);
+
 module.exports = {
-  db: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  },
+  db,
   port: process.env.PORT ?? 3000,
 };
