35f3e10fb35ef3f500340fb72575878fe97c3357
Benoît Hubert <benoithubert@gmail.com>
2024-04-21 18:08:55 +0200

    Finalize frontend dockerization

---
+++ README.md
@@ -0,0 +1,13 @@
+# Build
+
+## Backend image
+
+```
+docker build -t benoithubert/minimal-node-mysql-app:0.2.0 .
+```
+
+## Frontend image
+
+```
+docker build -t benoithubert/minimal-react-app:0.2.0 .
+```
--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,1 +1,8 @@
-export const serverUrl = import.meta.env.VITE_SERVER_URL ?? 'http://localhost:3000';
+// we'll use Vite's "mode" in order to determine the server default URL
+// localhost:3000 for development,
+// EMPTY for production
+const isProduction = import.meta.env.MODE === 'production';
+export const fallbackUrl = isProduction ? '' : 'http://localhost:3000';
+
+// we'll allow the user to override the server URL
+export const serverUrl = import.meta.env.VITE_SERVER_URL ?? fallbackUrl;
